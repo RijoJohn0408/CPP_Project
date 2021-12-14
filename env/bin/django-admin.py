0e0fa6f5c2bc4940e458700d8ff2617372b164c3
EC2 Default User <ec2-user@ip-172-31-93-14.ec2.internal>
2021-12-14 18:13:31 +0000

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/lms_school/school_learning_management/teaching_blog/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
